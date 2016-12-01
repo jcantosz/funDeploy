f1c0626f2bf667f5ddbbee9e403d818147436598
Jesse C. Antoszyk <jcantosz@us.ibm.com>
2016-12-01 14:34:05 -0500

    fun

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,3 +5,4 @@ wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key |
 echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
 sudo apt-get update
 sudo apt-get install cf-cli
+sudo apt-get install pip
