c06776ec41c964785b80492680e7b25c1f0db584
Adam King <rak@us.ibm.com>
2016-12-08 15:30:05 -0500

    qualify bin

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,17 +2,23 @@
 
 BIN_DIR="$(dirname $0)"
 
-"$BIN_DIR/curlStatus.sh"  "pending" "Deployment to bluemix started"
-
 cd tmp
 ls -l
+BIN_DIR="../$BIN_DIR"
 
-ansible-playbook ../tstbk.yml
+echo "Deployment to bluemix started"
+"$BIN_DIR/curlStatus.sh"  "pending" "Deployment to bluemix started"
+
+
+
+#ansible-playbook ../tstbk.yml
 rc=$?
 
 if [[ $rc != 0 ]]; then
+  echo "Deployment to bluemix failed"
   "$BIN_DIR/curlStatus.sh"  "failure" "Deployment to bluemix failed"
   exit $rc
 else
+  echo "Deployment to bluemix successful"
   "$BIN_DIR/curlStatus.sh"  "success" "Deployment to bluemix successful"
 fi
