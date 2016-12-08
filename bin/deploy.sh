22b809e20f86516ed3e3b883a56de94263eb9e96
Adam King <rak@us.ibm.com>
2016-12-08 14:45:17 -0500

    reorganize .travis.yml to run ansible as a deployment script

---
+++ bin/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BIN_DIR=$(dirname $0)
+
+"$BIN_DIR/curlStatus.sh"  "pending" "Deployment to bluemix started"
+
+ansible-playbook tstbk.yml
+rc=$?
+
+if [[ $rc != 0 ]]; then
+  "$BIN_DIR/curlStatus.sh"  "failure" "Deployment to bluemix failed"
+  exit $rc
+else
+  "$BIN_DIR/curlStatus.sh"  "success" "Deployment to bluemix successful"
+fi
